1a6014d0c4687ae4777a73a27e3a1d0fd8ae5833
kowasaur <kowasaur@gmail.com>
2021-01-19 12:38:12 +1000

    Updates

---
+++ _next/static/8q337cFbr3HMNKS4SjDGO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-62433674bfe03a65cf13.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2157553d569a4a4e7bf2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
